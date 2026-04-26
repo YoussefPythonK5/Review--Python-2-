f2c29014f8c733eb531ac3e54e68d844dcb4a1a3
Youssef <ya103857@gmail.com>
2026-04-26 22:00:28 +0300

    Add files via upload

---
+++ Review of text handling(2).py
@@ -0,0 +1,14 @@
+# lower
+name1 = "YOUSSEF"
+print (name1.lower())
+print ("*" *10)
+# upper
+name2 = "mohamed"
+print (name2.upper())
+# title
+print ("*" *10)
+name3 = "youssef amr"
+print (name3.title())
+# capitalize
+name4 = "ahmed Ali"
+print (name4.capitalize)
